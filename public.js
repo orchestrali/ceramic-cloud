12406a6b4a8fbc53f0c4d2680c8df1921eaae234
orchestrali <orchestrali@gmail.com>
2025-08-28 03:19:50 +0100

    step analysis

--- public.js
+++ public.js
@@ -70,7 +70,7 @@ function getcomplib(compid, w) {
       } else if (what === "compexperiment") {
         
       } else {
-        
+        analyzesteps();
       }
       
     }
@@ -86,6 +86,65 @@ function checkbit(value, bit) {
   return d % 2 === 1;
 }
 
+//take arrays of html and add them
+function displayanalysis(rows, cats) {
+  $("#rowcolumn").append(`<li class="fade">${places.slice(0,numbells)}</li>`); //
+  $("#catcolumn").append(`<li class="fade">(starting rounds)</li>`); //
+  
+  rows.forEach(h => {
+    $("#rowcolumn").append(h);
+  });
+  
+  cats.forEach(h => {
+    $("#catcolumn").append(h);
+  });
+}
+
+//examine each row for steps and highlight them
+function analyzesteps() {
+  let rowhtml = [];
+  let cathtml = [];
+  rowarr.forEach(r => {
+    let runs = findsteps(r);
+    if (runs.length === 0) {
+      let html = `<li>${rowstring(r)} </li>`;
+      let cat = `<li class="fade">none</li>`;
+      rowhtml.push(html);
+      cathtml.push(cat);
+    } else {
+      let html = `<li>`;
+      let prev;
+      let dir = 1;
+      for (let p = 1; p <= numbells; p++) {
+        let i = runs.findIndex(a => a.includes(p));
+        let b = bellnum(r[p-1]);
+        if (i === -1) {
+          if (prev) html += `</span>`;
+          html += b;
+          prev = null;
+        } else {
+          if (i === prev) {
+            html += b;
+          } else {
+            if (prev) {
+              html += `</span>`;
+              dir*=-1;
+            }
+            let c = dir === 1 ? "highlightblue" : "highlightgreen";
+            html += `<span class="${c}">`+b;
+            prev = i;
+          }
+        }
+      }
+      if (prev) html += `</span>`;
+      html += `</li>`;
+      let cat = `<li>${runs.length} step chunk(s)</li>`;
+      rowhtml.push(html);
+      cathtml.push(cat);
+    }
+  });
+  displayanalysis(rowhtml, cathtml);
+}
 
 // ************* BELLRINGING FUNCTIONS *************
 
@@ -97,3 +156,35 @@ function bellnum(c) {
   return places.indexOf(c)+1;
 }
 
+//calculate bell intervals
+//r is array
+function rowints(r, abs) {
+  let ii = [];
+  for (let i = 1; i <= r.length; i++) {
+    let d = r[i]-r[i-1];
+    abs ? ii.push(Math.abs(d)) : ii.push(d);
+  }
+  return ii;
+}
+
+//find "runs" (actually just steps)
+function findsteps(r) {
+  let dd = rowints(r, true);
+  
+  let runs = [];
+  let current = [];
+  for (let i = 0; i < dd.length; i++) {
+    let d = dd[i];
+    if (d === 1) {
+      if (current.length === 0) {
+        current.push(i+1,i+2);
+      } else {
+        current.push(i+2);
+      }
+    } else {
+      if (current.length) runs.push(current);
+      current = [];
+    }
+  }
+  return runs;
+}
